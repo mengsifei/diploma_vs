171bf2b32cd0bb677bad2da66d6210c8effcd11e
Sifei <victoriameng2013@gmail.com>
2024-04-11 12:48:34 +0300

    added dual version

--- datasets/Dataset.py
+++ datasets/Dataset.py
@@ -32,3 +32,46 @@ class CustomDataset(torch.utils.data.Dataset):
             'attention_mask': inputs['attention_mask'].flatten(),
             'labels': torch.FloatTensor(list(self.labels[index]))
         }
+    
+
+
+class CustomDatasetDual(torch.utils.data.Dataset):
+    def __init__(self, df, tokenizer, max_len=512):
+        self.tokenizer = tokenizer
+        self.df = df
+        self.essays = df['essay']
+        self.topics = df['prompt']
+        self.labels = df[['Task Response', 'Coherence and Cohesion', 'Lexical Resource', 'Grammatical Range and Accuracy']].values
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.essays)
+
+    def __getitem__(self, index):
+        essay_text = self.essays[index]
+        topic_text = self.topics[index]
+        essay_text = essay_text.replace("\n", f" [SEP] ")
+        essay_inputs = self.tokenizer(
+            essay_text,
+            max_length=self.max_len - 60, 
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt'
+        )
+        
+        topic_inputs = self.tokenizer(
+            topic_text,
+            max_length=60,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt'
+        )
+        
+        return {
+            'essay_input_ids': essay_inputs['input_ids'].flatten(),
+            'essay_attention_mask': essay_inputs['attention_mask'].flatten(),
+            'topic_input_ids': topic_inputs['input_ids'].flatten(),
+            'topic_attention_mask': topic_inputs['attention_mask'].flatten(),
+            'labels': torch.FloatTensor(self.labels[index])
+        }
+    
