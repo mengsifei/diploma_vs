15f1ec1225e95d406eddfa64224ba3bdeb1d47d9
Sifei <victoriameng2013@gmail.com>
2024-04-13 07:52:17 +0300

    electramodel

--- models/electra_baseline.py
+++ models/electra_baseline.py
@@ -1,12 +1,12 @@
 from models.poolings import *
 import torch
-from transformers import AutoModel
+from transformers import ElectraModel
 import torch.nn as nn
 
 # class ELECTRA(torch.nn.Module):
 #     def __init__(self):
 #         super(ELECTRA, self).__init__()
-#         self.electra = AutoModel.from_pretrained('google/electra-small-discriminator')
+#         self.electra = ElectraModel.from_pretrained('google/electra-small-discriminator')
 #         self.dropout = nn.Dropout(0.2)
 #         self.pooler = MeanPooling()
 #         self.out = nn.Linear(self.electra.config.hidden_size, 4)  # Ensure hidden_size matches
@@ -23,7 +23,7 @@ import torch.nn as nn
 class ELECTRA(torch.nn.Module):
     def __init__(self):
         super(ELECTRA, self).__init__()
-        self.electra = AutoModel.from_pretrained('google/electra-small-discriminator')
+        self.electra = ElectraModel.from_pretrained('google/electra-small-discriminator')
         self.dropout = nn.Dropout(0.2)
         self.pooler = MeanPooling()
         self.out = nn.Linear(self.electra.config.hidden_size, 4)  # Ensure hidden_size matches
@@ -73,7 +73,7 @@ class CrossAttention(nn.Module):
 class CustomELECTRA(nn.Module):
     def __init__(self, hidden_size=256):
         super(CustomELECTRA, self).__init__()
-        self.electra = AutoModel.from_pretrained('google/electra-small-discriminator')
+        self.electra = ElectraModel.from_pretrained('google/electra-small-discriminator')
         self.cross_attention = CrossAttention(hidden_size)
         self.pooler = MeanPooling()
         self.dropout = nn.Dropout(0.2)
