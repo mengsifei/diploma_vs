8b6562c2ed32b15b68572e4f1ec3022c7c957aaf
Sifei <victoriameng2013@gmail.com>
2024-04-26 12:55:13 +0300

    ..

--- models/electra_baseline.py
+++ models/electra_baseline.py
@@ -30,7 +30,7 @@ class BaseModel(nn.Module):
         self.out = nn.Linear(self.hidden_size * 2, num_labels)
     def get_model(self):
         if self.model_name == 'electra':
-            self.model = ElectraModel.from_pretrained('google/electra-small-discriminator')
+            self.model = ElectraModel.from_pretrained('google/electra-small-discriminator', output_hidden_states=True)
         elif self.model_name == 'bert':
             self.model = BertModel.from_pretrained('bert-base-cased')
         elif self.model_name == 'gpt':
@@ -45,7 +45,8 @@ class BaseModel(nn.Module):
         outputs = self.model(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
         hidden_states = outputs.hidden_states
         # layer4_output = self.pooler(self.linear4(hidden_states[3]), attention_mask)
-        layer8_output = self.pooler(self.linear8(hidden_states[7]), attention_mask)
+        print(len(hidden_states))
+        layer8_output = self.pooler(self.linear8(hidden_states[8]), attention_mask)
         layer12_output = self.pooler(self.linear12(hidden_states[-1]), attention_mask)
         concatenated_output = torch.cat((layer8_output, layer12_output), dim=-1)
         dropout_output = self.dropout(concatenated_output)
