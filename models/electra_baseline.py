ca0b721d3b92d3f39f4f4ca1bb49ca95174ae28e
Sifei <victoriameng2013@gmail.com>
2024-04-26 12:45:32 +0300

    concat812

--- models/electra_baseline.py
+++ models/electra_baseline.py
@@ -27,7 +27,7 @@ class BaseModel(nn.Module):
         self.linear8 = nn.Linear(self.hidden_size, self.hidden_size)
         self.linear12 = nn.Linear(self.hidden_size, self.hidden_size)
         self.dropout = nn.Dropout(hidden_dropout_prob)
-        self.out = nn.Linear(self.hidden_size * 3, num_labels)
+        self.out = nn.Linear(self.hidden_size * 2, num_labels)
     def get_model(self):
         if self.model_name == 'electra':
             self.model = ElectraModel.from_pretrained('google/electra-small-discriminator')
@@ -44,10 +44,10 @@ class BaseModel(nn.Module):
     def forward(self, input_ids, attention_mask, token_type_ids=None):
         outputs = self.model(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
         hidden_states = outputs.hidden_states
-        layer4_output = self.pooler(self.linear4(hidden_states[3]), attention_mask)
+        # layer4_output = self.pooler(self.linear4(hidden_states[3]), attention_mask)
         layer8_output = self.pooler(self.linear8(hidden_states[7]), attention_mask)
         layer12_output = self.pooler(self.linear12(hidden_states[-1]), attention_mask)
-        concatenated_output = torch.cat((layer4_output, layer8_output, layer12_output), dim=-1)
+        concatenated_output = torch.cat((layer8_output, layer12_output), dim=-1)
         dropout_output = self.dropout(concatenated_output)
         out = self.out(dropout_output)
         return out
