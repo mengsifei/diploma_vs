b5f4a26784ecc26e5856272bccd6b5b9ab0e7566
Sifei <victoriameng2013@gmail.com>
2024-05-15 20:11:43 +0300

    deleting port

--- app.py
+++ app.py
@@ -3,5 +3,4 @@ from my_app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    port = 8080
-    app.run(debug=True,host='0.0.0.0',port=port)
+    app.run(debug=True,host='0.0.0.0')
